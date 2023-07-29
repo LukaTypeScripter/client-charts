bdac5a3780b08af7e917d8996463940b3f5b69d8
luka shinjiakshvili <lukashinjikashvili85@gmail.com>
2023-07-29 21:47:42 +0400

    setting up backend next

--- src/screens/dashboard/Dashboard.tsx
+++ src/screens/dashboard/Dashboard.tsx
@@ -1,7 +1,8 @@
 
-import DashboardBox from '@/components/DashboardBox';
-import { Box,useMediaQuery,useTheme } from '@mui/material'
-import React from 'react'
+import { Box,useMediaQuery } from '@mui/material'
+import Row1 from './Row1'
+import Row2 from './Row2'
+import Row3 from './Row3'
 const gridTemplateLargeScreens = `
     "a b c"
     "a b c"
@@ -47,7 +48,6 @@ const gridTemplateSmallScreens = `
   "j"
 `;
 function Dashboard() {
-    const {palette} = useTheme()
     const isAboveMediumScreens = useMediaQuery("(min-width:1200px)")
   return (
     <Box width={'100%'} display={"grid"} gap={'1.5rem'}
@@ -63,26 +63,9 @@ function Dashboard() {
        }
     }
     >
-        <DashboardBox  gridArea={'a'}>
-        </DashboardBox>
-        <DashboardBox  gridArea={'b'}>
-        </DashboardBox>
-        <DashboardBox  gridArea={'c'}>
-        </DashboardBox>
-        <DashboardBox  gridArea={'d'}>
-        </DashboardBox>
-        <DashboardBox  gridArea={'e'}>
-        </DashboardBox>
-        <DashboardBox  gridArea={'f'}>
-        </DashboardBox>
-        <DashboardBox  gridArea={'g'}>
-        </DashboardBox>
-        <DashboardBox  gridArea={'h'}>
-        </DashboardBox>
-        <DashboardBox  gridArea={'i'}>
-        </DashboardBox>
-        <DashboardBox  gridArea={'j'}>
-        </DashboardBox>
+       <Row1 />
+        <Row2 />
+        <Row3 />
 
     </Box>
   )
