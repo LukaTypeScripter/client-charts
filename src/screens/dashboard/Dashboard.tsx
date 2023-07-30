f2346a27f9ad33497a002ca27f66fba5a0acf40a
luka shinjiakshvili <lukashinjikashvili85@gmail.com>
2023-07-30 17:44:27 +0400

    added first row charts

--- src/screens/dashboard/Dashboard.tsx
+++ src/screens/dashboard/Dashboard.tsx
@@ -50,17 +50,22 @@ const gridTemplateSmallScreens = `
 function Dashboard() {
     const isAboveMediumScreens = useMediaQuery("(min-width:1200px)")
   return (
-    <Box width={'100%'} display={"grid"} gap={'1.5rem'}
+    <Box width={"100%"}
+    height={"100%"}
+    display={"grid"}
+    gap={"1.5rem"}
     sx={
-       isAboveMediumScreens ? {
-        gridTemplateColumns:"repeat(3,minmax(370px,1fr))",
-        gridTemplateRows:"repeat(10,minmax(60px,1fr))",
-        gridTemplateAreas:   gridTemplateLargeScreens,
-       }: {
-        gridTemplateAreas:gridTemplateSmallScreens,
-        gridAutoColumns:"1fr",
-        gridAutoRows:"80px",
-       }
+      isAboveMediumScreens
+        ? {
+            gridTemplateColumns: "repeat(3, minmax(370px, 1fr))",
+            gridTemplateRows: "repeat(10, minmax(60px, 1fr))", // Adjust the number of rows here
+            gridTemplateAreas: gridTemplateLargeScreens,
+          }
+        : {
+            gridTemplateAreas: gridTemplateSmallScreens,
+            gridAutoColumns: "1fr",
+            gridAutoRows: "80px",
+          }
     }
     >
        <Row1 />
@@ -68,7 +73,8 @@ function Dashboard() {
         <Row3 />
 
     </Box>
-  )
+  ) 
 }
 
 export default Dashboard
+
---
+++ src/state/types.ts
@@ -0,0 +1,31 @@
+export interface ExpensesByCategory {
+  salaries: number;
+  supplies: number;
+  services: number;
+}
+export interface Month {
+  id: string;
+  month: string;
+  revenue: number;
+  nonOperationalExpenses: number;
+  operationalExpenses: number;
+  expenses:number
+}
+export interface Day {
+    id: string;
+    date:string;
+    month: string;
+    revenue: number;
+  }
+
+export interface GetKpisResponse {
+  id: string;
+  _id: string;
+  __v: number;
+  totalProfit: number;
+  totalRevenue: number;
+  totalExpenses: number;
+  expensesByCategory: ExpensesByCategory;
+  monthlyData: Array<Month>;
+  dailyData:Array<Day>
+}
